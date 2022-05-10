6c498297859b694e42e953abb7bd0f648289bc6d
metehanmart <metehanmart@gmail.com>
2022-05-10 19:49:02 +0300

    grafik oluşturuldu

--- akademikArama/Controllers/KullaniciController.cs
+++ akademikArama/Controllers/KullaniciController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Neo4j.Driver;
 using akademikArama.Services;
 #nullable enable
 namespace akademikArama.Controllers
